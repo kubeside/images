99865d3eb31bae15677cb16b10a39c8c465e82bb
MoreRustLessAccess <179813162+MoreRustLessAccess@users.noreply.github.com>
2026-09-10 13:12:03 +0200

    opnsense module for metro

---
+++ images/metro/modules/opnsense.js
@@ -0,0 +1,66 @@
+const { OPNSENSE_API_URL, OPNSENSE_AUTH } = process.env;
+
+export const metrics = [
+    {
+        name: "opnsense_dhcp_leases_per_interface",
+        type: "gauge",
+        description: "The amount of DHCP leases issued per subnet",
+        collect: async () => {
+            const r = await fetch(`${OPNSENSE_API_URL}/api/services/dhcpd/lease`, {headers: {Authorization: OPNSENSE_AUTH}});
+
+            const response = await r.json();
+            if (r.status !== 200) {
+                throw new Error(response);
+            }
+            const leases = response.rows;
+            const interfaceNameToLeaseCount = {};
+            const now = Date.now() / 1000;
+
+            leases.forEach(lease => {
+                const isStatic = lease.is_reserved.length !== 0;
+                if (isStatic) {
+                    return;
+                }
+                if (now > lease.expire) {
+                    return;
+                }
+                interfaceNameToLeaseCount[lease.if_name] = (interfaceNameToLeaseCount[lease.if_name] ?? 0) + 1;
+            });
+            const metrics = Object.keys(interfaceNameToLeaseCount).map(interface_name => {
+                return {
+                    value: interfaceNameToLeaseCount[interface_name],
+                    params: {
+                        interface_name
+                    }
+                };
+            });
+            return metrics;
+        }
+    },
+    {
+        name: "opnsense_service_status",
+        type: "gauge",
+        description: "Service status. 1 is running, 0 is not",
+        collect: async () => {
+            const r = await fetch(`${OPNSENSE_API_URL}/api/core/service/search`, {headers: {Authorization: OPNSENSE_AUTH}});
+
+            const response = await r.json();
+            if (r.status !== 200) {
+                throw new Error(response);
+            }
+
+            const services = response.data;
+
+            return services.map(service => {
+                return {
+                    value: service.running,
+                    params: {
+                        id: service.id,
+                        name: service.name,
+                    }
+                };
+            });
+
+        }
+    }
+];
